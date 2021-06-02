64446fc43593432b79a177dd9c9f737bb3b6399d
ManuelRevuelto <manu.revuelto1111@gmail.com>
2021-06-03 00:01:07 +0200

    comitEntrga

--- src/app/pages/modules/home/home.component.ts
+++ src/app/pages/modules/home/home.component.ts
@@ -3,11 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  template: `
+    <img src="../../../fotos/CEBO.jpg" alt="avatarAlumno">
+  `
 })
 export class HomeComponent implements OnInit {
+  dni = '25359770Q';
+  lugar = 'Zaragoza';
+  fechnac = '13/10/2001';
 
-  constructor() { }
+  domicilio = 'Calle Sangenis, 60';
+  telefono = '987654321';
+  nacionalidad = 'Español';
+  email = 'manu@gmail.com';
+  
+  cebo = '../fotos/CEBO.jpg';
+
+  constructor() { }  
 
   ngOnInit(): void {
   }
--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarioComponent } from '../pages/modules/calendario/calendario.component';
 import { EvaluacionComponent } from '../pages/modules/evaluacion/evaluacion.component';
 import { HomeComponent } from '../pages/modules/home/home.component';
 import { IncidenciasComponent } from '../pages/modules/incidencias/incidencias.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
 	{ path: '', redirectTo: '/home', pathMatch: 'full'},
 	{ path: 'home', component: HomeComponent},
 	{ path: 'matriculas', component: MatriculasComponent},
+	{ path: 'calendario', component: CalendarioComponent},
 	{ path: 'incidencias', component: IncidenciasComponent},
 	{ path: 'evaluacion', component: EvaluacionComponent},
 	{ path: 'login', loadChildren: () => import('../pages/modules/login/login.module').then(m => m.LoginModule) },
