1bf9d23f8ac467e4c56f9ba8587419aca8b37603
Manuel Revuelto <manu.revuelto1111@gmail.com>
2023-01-14 16:19:41 +0100

    Crear rutas api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\IncidentController;
+use App\Http\Controllers\MarkController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth')->group(function () {
+
+});
+
+Route::resource('asignaturas', SubjectController::class);
+Route::resource('faltas', AttendanceController::class);
+Route::resource('incidencias', IncidentController::class);
+Route::resource('notas', MarkController::class);
