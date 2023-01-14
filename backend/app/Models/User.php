1d062785e2a9a49ca6635b1f4909e2c3ac64dcd9
Manuel Revuelto <manu.revuelto1111@gmail.com>
2023-01-14 17:38:26 +0100

    Funcion login por username Relacion con estudiantes

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -32,12 +32,13 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function findForPassport($username)
+    {
+        return $this->where('username', $username)->first();
+    }
+
+    function Student()
+    {
+        return $this->hasOne(Student::class, 'id_user', 'id');
+    }
 }
