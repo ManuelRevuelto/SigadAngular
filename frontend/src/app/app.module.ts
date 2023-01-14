77c447f23771ac139a899e229cc4783a15450ad9
Manuel Revuelto <manu.revuelto1111@gmail.com>
2023-01-14 19:12:45 +0100

    Cambiar nombre de la carpeta

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,10 +8,10 @@ import { AuthModule } from '@auth0/auth0-angular';
 import { environment as env, environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginButtonComponent } from './components/login-button/login-button.component';
-import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
-import { AuthenticationButtonComponent } from './components/authentication-button/authentication-button.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginButtonComponent } from './modules/login-button/login-button.component';
+import { LogoutButtonComponent } from './modules/logout-button/logout-button.component';
+import { AuthenticationButtonComponent } from './modules/authentication-button/authentication-button.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms'
 
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -32,11 +32,6 @@ import { AngularFireModule } from '@angular/fire';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    AuthModule.forRoot({
-      ...env.auth,
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
